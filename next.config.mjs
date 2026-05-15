c7d9b86268d792700a65a8af909a7bd712d202ab
Ruculs <lheyculit@gmail.com>
2026-05-16 03:10:07 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,13 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    workerThreads: false,
+    cpus: 1,
+  },
 };
 
 export default nextConfig;
